33757a69292b23c67998faddae620a3161b5bc9b
delm83 <delm83@googlemail.com>
2024-09-02 10:54:50 +0100

    updated canvas with text and border for play area

--- public/game.mjs
+++ public/game.mjs
@@ -6,4 +6,12 @@ const socket = io();
 const canvas = document.getElementById('game-window');
 const ctx = canvas.getContext('2d');
 
+ctx.font = '30px Fantasy';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = 'white';
+ctx.strokeRect(3, 70, canvas.width-6, canvas.height-73);
+ctx.fillStyle = 'white';
+ctx.fillText('Controls: WASD', 5, 50);
+ctx.fillText('Grab the coin!!', 250, 50);
+ctx.fillText('Rank: ', 500, 50);
+ctx.fillText
