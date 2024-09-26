9e56bef8d70824ebb3346519ae4349d3967e3a7a
delm83 <delm83@googlemail.com>
2024-09-26 10:38:46 +0100

    draw images at random places on canvas

--- public/game.mjs
+++ public/game.mjs
@@ -1,5 +1,5 @@
-//import Player from './Player.mjs';
-//import Collectible from './Collectible.mjs';
+import Player from './Player.mjs';
+import Collectible from './Collectible.mjs';
 
 
 const socket = io();
@@ -12,22 +12,22 @@ ctx.strokeStyle = 'white';
 ctx.strokeRect(3, 70, canvas.width-6, canvas.height-73);
 ctx.fillStyle = 'white';
 ctx.fillText('Controls: WASD', 5, 50);
-ctx.fillText('Collect the stars!!', 250, 50);
+ctx.fillText('Collect The Stars!!', 250, 50);
 ctx.fillText('Rank: ', 500, 50);
 
 
 const init =()=>{
 let img = new Image();
 img.src = "assets/star.png";
-img.onload =()=>ctx.drawImage(img, 80, 80, 20, 20);
+img.onload =()=>ctx.drawImage(img, 3 + Math.random()*(canvas.width-26), 70 + Math.random()*(canvas.height-93), 20, 20);
 
 let img2 = new Image();
 img2.src = "assets/bluevirus.png"
-img2.onload =()=>ctx.drawImage(img2, 80, 280, 40, 40);
+img2.onload =()=>ctx.drawImage(img2, 3 + Math.random()*(canvas.width-48), 70 + Math.random()*(canvas.height-115), 40, 40);
 
 let img3 = new Image();
 img3.src = "assets/redvirus.png"
-img3.onload =()=>ctx.drawImage(img3, 280, 280, 40, 40);
+img3.onload =()=>ctx.drawImage(img3, 3 + Math.random()*(canvas.width-48), 70 + Math.random()*(canvas.height-115), 40, 40);
 }
 
 init();
