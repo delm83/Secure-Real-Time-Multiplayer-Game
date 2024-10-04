dd35de9847fe686a09236c258debbbcf20079ac1
delm83 <delm83@googlemail.com>
2024-10-04 13:49:25 +0100

    clear and redraw display when animation moves (using canvas.width=canvas.width;) as clearRect doesnt redraw for some reason

--- public/game.mjs
+++ public/game.mjs
@@ -1,36 +1,26 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
 
-
 const socket = io();
 const canvas = document.getElementById('game-window');
 const ctx = canvas.getContext('2d');
 let dir = null;
-
-ctx.font = '30px Fantasy';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = 'white';
-ctx.strokeRect(3, 70, canvas.width-6, canvas.height-73);
-ctx.fillStyle = 'white';
-ctx.fillText('Controls: WASD', 5, 50);
-ctx.fillText('Collect The Stars!!', 250, 50);
-ctx.fillText('Rank: ', 500, 50);
+let x = 3 + Math.random()*(canvas.width-48);
+let y = 70 + Math.random()*(canvas.height-115);
+let player = new Player({x:x, y:y, score:0, id:0});
+let playerImg = new Image();
+playerImg.src = "assets/bluemoon.png"
+let starImg = new Image();
+starImg.src = "assets/star.png";
 
 // canvas width: 640, canvas height: 480
+const init=()=>{
 socket.on('newPlayer', (id, stardata)=>{
-    let x = 3 + Math.random()*(canvas.width-48);
-    let y = 70 + Math.random()*(canvas.height-115);
-    let player = new Player({x:x, y:y, score:0, id:id});
-    let playerImg = new Image();
-    playerImg.src = "assets/bluemoon.png"
-    playerImg.onload =()=>ctx.drawImage(playerImg, x, y, 40, 40);
-    console.log('x: '+stardata.x)
-    console.log('y: '+stardata.y)
-    console.log('value: '+stardata.value)
-    console.log('id: '+stardata.id)
-    let starImg = new Image();
-    starImg.src = "assets/star.png";
-    starImg.onload =()=>ctx.drawImage(starImg, stardata.x, stardata.y, 20, 20);
+    player.id = id;
+    
+    //playerImg.onload =()=>ctx.drawImage(playerImg, x, y, 40, 40);
+    
+    //starImg.onload =()=>ctx.drawImage(starImg, stardata.x, stardata.y, 20, 20);
 
     window.addEventListener("keydown", ({ key }) => {
         if(key == 'W' || key == 'w'){
@@ -60,10 +50,26 @@ socket.on('newPlayer', (id, stardata)=>{
         player.movePlayer(dir, 8);
         console.log('x is '+player.x);
         console.log('y is '+player.y);
-        ctx.drawImage(playerImg, player.x, player.y, 40, 40);
       });
+      
 });
+requestAnimationFrame(update);
+}
 
+const update=()=>{
+    canvas.width=canvas.width;
+    ctx.font = '30px Fantasy';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(3, 70, canvas.width-6, canvas.height-73);
+    ctx.fillStyle = 'white';
+    ctx.fillText('Controls: WASD', 5, 50);
+    ctx.fillText('Collect The Stars!!', 250, 50);
+    ctx.fillText('Rank: ', 500, 50);
+    ctx.drawImage(playerImg, player.x, player.y, 40, 40);
+    requestAnimationFrame(update);
+     }  
+init();
 /*
 let starImg = new Image();
 starImg.src = "assets/star.png";
